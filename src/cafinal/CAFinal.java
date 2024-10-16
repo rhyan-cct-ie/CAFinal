c391bbf2bd0ba8ac95a1801f0a03ec702e954f07
rhyan-cct-ie <sba24005@student.cct.ie>
2024-10-16 17:08:28 +0100

    Added comments on my file reader

--- src/cafinal/CAFinal.java
+++ src/cafinal/CAFinal.java
@@ -8,27 +8,28 @@ package cafinal;
  *
  * @author reg05
  */
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.BufferedReader;//Imports a BufferedReader class to read the file efficiently or faster
+import java.io.FileReader;//handle the basic charecter-reading on my txt file
+import java.io.IOException;//This is to help error-handling in the program as many issues can arise from reading and writing to a file
 
-public class CAFinal {
+public class CAFinal {//
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        //The file path is written below
-        String myFile = "C:\\Users\\reg05\\CCT\\customers.txt";//This tells the program where the txt file is located 
+        /*The line below declares a String variable named myFile and assigns it the name 
+        of the file to be read, customers.txt. This file should be in the same directory as the program or provide a full path.*/
+        String myFile = "C:\\Users\\reg05\\CCT\\customers.txt";
         
         try (BufferedReader br = new BufferedReader(new FileReader(myFile))){
-            String line;
-            while ((line = br.readLine()) != null) {
+            String line;//This line declares a String variable named line, which holds each line of text read from the txt file.
+            while ((line = br.readLine()) != null) {//The while loop here reads each line until it reaches the end of the file and causes it to exit
                 //validation proces needs to be done here
                 System.out.println(line);
                 
             }
-        } catch (IOException e){
+        } catch (IOException e){//This line starts the catch block to handle any IOException that might occur during the operation.
                 e.printStackTrace();
         }
     }
