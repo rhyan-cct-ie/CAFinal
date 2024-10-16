03949080b657f43eb50dea0b66e9d7fbc39b21ef
rhyan-cct-ie <sba24005@student.cct.ie>
2024-10-16 16:36:27 +0100

    Created a TXT file reader

--- src/cafinal/CAFinal.java
+++ src/cafinal/CAFinal.java
@@ -8,13 +8,29 @@ package cafinal;
  *
  * @author reg05
  */
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class CAFinal {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //The file path is written below
+        String myFile = "C:\\Users\\reg05\\CCT\\customers.txt";//This tells the program where the txt file is located 
+        
+        try (BufferedReader br = new BufferedReader(new FileReader(myFile))){
+            String line;
+            while ((line = br.readLine()) != null) {
+                //validation proces needs to be done here
+                System.out.println(line);
+                
+            }
+        } catch (IOException e){
+                e.printStackTrace();
+        }
     }
     
 }
