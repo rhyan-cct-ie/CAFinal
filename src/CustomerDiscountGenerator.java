8cc2cb2594c04915c0d6c5dc3fd59b8826be0249
rhyan-cct-ie <sba24005@student.cct.ie>
2024-10-26 20:49:55 +0100

    Removed file writer from main class

--- src/CustomerDiscountGenerator.java
+++ src/CustomerDiscountGenerator.java
@@ -25,26 +25,6 @@ public class CustomerDiscountGenerator {// The name of the Program
         String customerFile = "C:\\Users\\reg05\\CCT\\customers.txt";//The file path where the program reads from.
         String outputCustomerFile = "C:\\Users\\reg05\\CCT\\customersdiscount.txt";//The file path where the programs writ to.
         
-        // Write valid customers to the output file as customersdiscount.txt
-        try (BufferedWriter bw = new BufferedWriter(new FileWriter(outputCustomerFile))) {
-            //Opens the output file for writing. The Buffered writer warps around FileWriter for efficient writing and the file is
-            //automaticall closed.
-            for (Customer customer : customers) { //For loop is used for simplicity and readability. This iterates through the list of valid customers.
-                
-                //Calculate the FINAL VALUE using the Discount Calculator
-                DiscountCalculator calculator = new DiscountCalculator(customer);
-                double finalValue = calculator.calculateDiscountedValue();
-                
-                bw.write(customer.getFirstName() + " " + customer.getSecondName());/*Writes customer's full name to output file.*/
-                bw.newLine(); // Move to the next line
-                bw.write(String.valueOf(finalValue));//BufferedWriter only writes in text therefore a double needs to be converted into a string
-                bw.newLine();//Space to separate each customer's data.
-            }
-            System.out.println("List of Customer Discounts written to " + outputCustomerFile);//Confirmation message indicating that the output file was successful.
-        } catch (IOException e) {//Catch block for handling errors for writing a file, such as file permission issues.
-            System.out.println("Error writing to file: " + e.getMessage());
-        }
-        
         // Validate that the file exists, is readable, or not found
         File outputFile = new File(outputCustomerFile);
         if (outputFile.exists()) {
